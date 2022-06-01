6b849c289ed114d01ad8ebd4c74b621104d96e03
sidonne <kongnyuy2002@gmail.com>
2022-06-01 12:10:18 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "cv-ionic-ff6c1",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
 };
 
 /*
